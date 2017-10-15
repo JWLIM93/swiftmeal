b566e51323227f814e553a8472a542d1785e4e8e
Justin Fong <justin.fongjunhao@outlook.com>
2017-10-16 02:05:24 +0800

    Base Code

---
+++ map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianVzdGxuZiIsImEiOiJjajhzeXhrdWIwZ3p2MnFvNDFvZnF4cnh3In0.Fyue5JNW7KqTtDvd_fLGhQ';
+var map = new mapboxgl.Map({
+container: 'map',
+zoom: 13,
+center: [103.860330, 1.283951],
+style: 'mapbox://styles/mapbox/dark-v9'
+});
