22d06d23c794eb7e3160d6a82b69415eb5000dce
Justin Fong <justin.fongjunhao@outlook.com>
2017-10-17 22:39:55 +0800

    Clean up base code, implemented Login Screen.

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoianVzdGxuZiIsImEiOiJjajhzeXhrdWIwZ3p2MnFvNDFvZnF4cnh3In0.Fyue5JNW7KqTtDvd_fLGhQ';
 var map = new mapboxgl.Map({
 container: 'map',
-zoom: 13,
+zoom: 14,
 center: [103.860330, 1.283951],
-style: 'mapbox://styles/mapbox/dark-v9'
+style: 'mapbox://styles/justlnf/cj8vbviqsf3de2rqnn2ns0a9p'
 });
