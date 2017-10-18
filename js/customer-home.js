2b7fe9fe1c5230d9ebd677b154ad971ad6d56ee8
Justin Fong <justin.fongjunhao@outlook.com>
2017-10-19 03:43:08 +0800

    Code Changes, nothing major

--- js/customer-home.js
+++ js/customer-home.js
@@ -1,2 +1,6 @@
 var toolbar = mdc.toolbar.MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
 toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
+
+let menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
+// Add event listener to some button to toggle the menu on and off.
+document.querySelector('.mdc-button').addEventListener('click', () => menu.open = !menu.open);
