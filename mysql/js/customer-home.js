18d37504a6498e076ae95910d31470a6d7a328cb
Justin Fong <justin.fongjunhao@outlook.com>
2017-10-26 02:06:36 +0800

    Implement customer home and general code fixes

--- mysql/js/customer-home.js
+++ mysql/js/customer-home.js
@@ -2,6 +2,34 @@ var toolbar = mdc.toolbar.MDCToolbar.attachTo(document.querySelector('.mdc-toolb
 toolbar.fixedAdjustElement = document.querySelector('.mdc-toolbar-fixed-adjust');
 
 let menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
-menu.addEventListener('click', () =>)
-// Add event listener to some button to toggle the menu on and off.
-document.querySelector('.mdc-button').addEventListener('click', () => menu.open = !menu.open);
+
+// Profile menu - Individual menu selection listenr
+document.querySelector('#menu-edit-profile-button').addEventListener('click', () => {
+    alert(`Edit Profile Clicked!`);
+})
+
+document.querySelector('#menu-show-history-button').addEventListener('click', () => {
+    alert(`Show History Clicked!`);
+})
+
+document.querySelector('#menu-change-password-button').addEventListener('click', () => {
+    alert(`Change Password Clicked!`);
+})
+
+document.querySelector('#menu-logout-button').addEventListener('click', () => {
+    window.location = "/mysql/index.html";
+})
+
+// Toolbar button selection listener
+document.querySelector('#notifications-nav').addEventListener('click', () => {
+    
+})
+
+document.querySelector('#friends-list-nav').addEventListener('click', () => {
+
+})
+
+document.querySelector('#customer-profile').addEventListener('click', () => menu.open = !menu.open);
+
+// Toggle buttons
+mdc.iconToggle.MDCIconToggle.attachTo(document.querySelector('.mdc-icon-toggle'));
--- mysql/js/register.js
+++ mysql/js/register.js
@@ -1,5 +1,5 @@
 const select = new mdc.select.MDCSelect(document.querySelector('.mdc-select'));
 select.listen('MDCSelect:change', () => {
   alert(`Selected "${select.selectedOptions[0].textContent}" at index ${select.selectedIndex} ` +
-        `with value "${select.value}"`);
+    `with value "${select.value}"`);
 });
